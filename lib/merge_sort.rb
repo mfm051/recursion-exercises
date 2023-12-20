a0ec563beeb17b2d048fa9d3d727ddc36f295c4c
mateus <mateusmatos051@gmail.com>
2023-12-19 22:04:57 -0300

    Add method to split array
    Move methods to Array class

--- lib/merge_sort.rb
+++ lib/merge_sort.rb
@@ -1,18 +1,28 @@
-def merge_sort(array)
-  return array if array.one?
+class Array
+  def split_in_half
+    half_index = size / 2.0
 
-  left = merge_sort(array[..array.size / 2 - 1])
-  right = merge_sort(array[array.size / 2..])
+    left = first(half_index.floor)
+    right = last(half_index.ceil)
 
-  result = []
-
-  until result.size == array.size
-    next result << [left.shift, right.shift].compact[0] if [left[0], right[0]].any?(nil)
-
-    next result << right.shift if right[0] < left[0]
-
-    result << left.shift
+    [left, right]
   end
 
-  result
+  def merge_sort
+    return self if one?
+
+    left, right = split_in_half.map(&:merge_sort)
+
+    result = []
+
+    until result.size == size
+      next result << [left.shift, right.shift].compact[0] unless [left[0], right[0]].all?
+
+      next result << right.shift if right[0] < left[0]
+
+      result << left.shift
+    end
+
+    result
+  end
 end
